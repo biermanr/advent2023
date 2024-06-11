a018ae8a2ef0c3a79d54665c04f2ae360229095d
Rob Bierman <rbierman@princeton.edu>
2024-06-11 15:07:33 -0400

    feat: Day 4 part1 and part2 in rust

--- src/day04.rs
+++ src/day04.rs
@@ -1,6 +1,6 @@
 use std::fs;
 use itertools::Itertools;
-use std::collections::HashSet;
+use std::collections::{HashSet,HashMap};
 
 pub fn run_part1(data: &str) {
     let mut total_winnings = 0;
@@ -34,3 +34,45 @@ pub fn run_part1(data: &str) {
     println!("{:?}",total_winnings);
 }
 
+pub fn run_part2(data: &str) {
+    let mut wins_per_card: HashMap<usize, usize> = HashMap::new();
+
+    for line in fs::read_to_string(data).unwrap().lines() {
+        let (card_num, l) = line
+            .splitn(2, ": ")
+            .collect_tuple()
+            .unwrap();
+
+        let card_num: usize = card_num
+            .split_whitespace()
+            .last()
+            .unwrap()
+            .parse()
+            .unwrap();
+
+        *wins_per_card.entry(card_num).or_insert(0) += 1;
+
+        let (nums,win_nums) = l.splitn(2, " | ").collect_tuple().unwrap();
+
+        let nums: HashSet<u32> = nums
+            .trim()
+            .split_whitespace()
+            .filter_map(|c| Some(c.parse::<u32>().unwrap()))
+            .collect();
+
+        let win_nums: HashSet<u32> = win_nums
+            .trim()
+            .split_whitespace()
+            .filter_map(|c| Some(c.parse::<u32>().unwrap()))
+            .collect();
+
+        let num_wins = nums.intersection(&win_nums).count();
+
+        for i in 1..(num_wins+1) {
+            *wins_per_card.entry(card_num+i).or_insert(0) += wins_per_card[&card_num];
+        }
+    }
+
+    let total_wins: usize = wins_per_card.values().sum();
+    println!("{}",total_wins);
+}
