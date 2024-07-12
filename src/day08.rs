9a3e408cf389fc660a93e353febaae12fd4e5996
Rob Bierman <rbierman@princeton.edu>
2024-07-12 16:06:34 -0400

    feat: finished day8 part2 with LCM

--- src/day08.rs
+++ src/day08.rs
@@ -60,6 +60,8 @@ pub fn run_part2(data: &str) {
 
     let mut num_steps = 0;
 
+    let mut finished_cycles = 0;
+    let mut cycle_lens: HashMap<usize, usize> = HashMap::new();
 
     'outer: loop {
         for d in directions.chars() {
@@ -69,11 +71,52 @@ pub fn run_part2(data: &str) {
                 .map(|n| if d == 'L'  { nodes[n].0 } else { nodes[n].1 })
                 .collect();
 
-            if curr_nodes.iter().all(|n| n.chars().last() == Some('Z')) {
+            for (i,n) in curr_nodes.iter().enumerate() {
+                if n.chars().last() == Some('Z') {
+                    cycle_lens.entry(i).or_insert(num_steps);
+                    finished_cycles += 1;
+                    println!("Cycle {:?} took {:?} steps to finish",i,num_steps);
+                }
+            }
+
+            if finished_cycles == curr_nodes.len() {
                 break 'outer;
             }
         }
     }
-    println!("{:?}",num_steps);
+
+    let answer = lcm(cycle_lens.into_values().collect());
+    println!("{:?}",answer);
 
 }
+
+// STOLEN FROM https://www.andyloree.com/blog/2022/12/11/least-common-multiple-vect-rust/
+/// Least common multiple vec of numbers
+fn lcm(numbers: Vec<usize>) -> usize {
+    let mut temp = numbers.clone();
+    
+    // check all the same
+    loop {
+        let mut same = true;
+
+        for idx in 1..temp.len() {
+            if temp[0] != temp[idx] {
+                same = false;
+                break;
+            }
+        }
+
+        if same {
+            return temp[0];
+        }
+
+        // Find lowest index
+        match temp.iter().enumerate().min_by(|(_, a), (_, b)| a.cmp(b)).map(|(index, _)| index) {
+            Some(idx) => {
+                temp[idx] = temp[idx] + numbers[idx];
+            },
+            None => panic!("Not possible")
+        }
+    }
+}
+
